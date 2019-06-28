52d8f3f3d88a1babaf0e1690d7d0ab2bc977512a
evgo <evgo2017@qq.com>
2019-06-28 23:53:06 +0800

    add choose part
    You can download some of the episodes but only when you input only one vedio
    你可以下载部分剧集，但是只有当你只输入了一部视频或者番剧的时候

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * 参考资料：
  * https://github.com/Xmader/bilitwin
  * https://github.com/blogwy/BilibiliVideoDownload
@@ -6,13 +6,14 @@
 const readlineSync = require('readline-sync')
     , rp = require('request-promise')
     , fs = require('fs')
+    , cheerio = require('cheerio')
 
 /* 
- * options：
- * SESSDATA: 控制台->Application->Storage->Cookies->SESSDATA，改成自己的
- * qn：quality，最高视频清晰度，返回的 qn <= 设置的 qn
- */
-const SESSDATA = 'bd3a8286%2C1561801818%2C67b64751'
+* options：
+* SESSDATA: 控制台->Application->Storage->Cookies->SESSDATA，改成自己的
+* qn：quality，最高视频清晰度，返回的 qn <= 设置的 qn
+*/
+const SESSDATA = ''
     , QN = 116
     , REFERER = 'https://www.bilibili.com'
     , HEADERS = {
@@ -28,7 +29,11 @@ class Info {
         const { aid, title, pages } = JSON.parse(vedioInfo).data
 
         return Promise.all(pages.map(async page => {
-            const { part, cid } = page
+            const { cid } = page
+                , dir = pages.length > 1 ? title : ''
+
+            let { part } = page
+            part = part ? part : title
 
             let downloadInfo = await rp({
                 uri: `https://api.bilibili.com/x/player/playurl?avid=${aid}&cid=${cid}&qn=${QN}&otype=json`,
@@ -37,19 +42,22 @@ class Info {
 
             let durl = JSON.parse(downloadInfo).data.durl
             if (durl.length < 2) {
+                // 无分段
                 return [{
-                    dir: '',
+                    dir,
                     title,
                     part,
+                    out: part,
                     url: durl[0].url
                 }]
             } else {
+                // 有分段
                 return durl.map((value, index) => {
                     return {
-                        dir: title,
+                        dir,
                         title,
                         part,
-                        index,
+                        out: `${part}-${index}`,
                         url: value.url
                     }
                 })
@@ -57,12 +65,21 @@ class Info {
         }))
     }
 
-    static async Bangumi(name, ss) {
+    static async Bangumi(ss) {
         let bangumiInfo = await rp(`https://api.bilibili.com/pgc/web/season/section?season_id=${ss}`)
         const pages = JSON.parse(bangumiInfo).result.main_section.episodes
 
         return Promise.all(pages.map(async page => {
-            const { id, title, long_title } = page
+            const { id, title: episode, long_title } = page
+                , $ = await rp({
+                    uri: `https://www.bilibili.com/bangumi/media/md${ss}`,
+                    headers: HEADERS,
+                    gzip: true,
+                    transform: function (body) {
+                        return cheerio.load(body);
+                    }
+                })
+            let title = '1'
 
             let downloadInfo = await rp({
                 uri: `https://api.bilibili.com/pgc/player/web/playurl?ep_id=${id}&qn=${QN}&otype=json`,
@@ -72,161 +89,124 @@ class Info {
             let durl = JSON.parse(downloadInfo).result.durl
             if (durl.length < 2) {
                 return [{
-                    dir: name,
-                    index: title,
+                    dir: title,
+                    title,
                     part: long_title,
-                    out: `第${title}集-${long_title}`,
+                    out: `第${episode}集-${long_title}`,
                     url: durl[0].url
                 }]
             } else {
                 return durl.map((value, index) => {
                     return {
-                        dir: name,
-                        index,
-                        out: `第${title}集-${long_title}-${index}`,
+                        dir: title,
+                        title,
+                        part: long_title,
+                        out: `第${episode}集-${long_title}-${index}`,
                         url: value.url
                     }
                 })
             }
         }))
     }
+
+    static show(infos) {
+        console.log(`\n`)
+        if (infos.length < 2) {
+            console.log(`[1] ALL`)
+            infos[0].map((pages, index) => {
+                index += 2
+                console.log(`[${index}] ${pages[0].part}`)
+            })
+            console.log(`[0] CANCEL`)
+        } else {
+            infos.map((info, index) => {
+                console.log(`[${index}] ${info[0][0].title}`)
+                index++
+            })
+        }
+        console.log(`\n`)
+    }
+
+    static async get(type) {
+        const sss = readlineSync.question('请输入视频的 av 号或者番剧的 season_id（以空格分隔）:').split(' ')
+        if (type == '0') {
+            return Promise.all(avs.map(async av => await Info.Vedio(av)))
+        } else if (type == '1') {
+            return Promise.all(sss.map(async ss => await Info.Bangumi(ss)))
+        } else {
+            return []
+        }
+    }
 }
 
 class Exporter {
     static IDM(infos) {
         let data = []
-        infos.map(info => {
-            info.map(pages => {
-                pages.map(page => {
-                    data.push(`<\r\n${page.url}\r\nreferer: ${REFERER}\r\n>\r\n`)
-                })
-            })
-        })
-        fs.appendFile('./download.ef2', data.join(','), () => {
+        infos.map(info => info.map(pages => pages.map(page => {
+            data.push(`<\r\n${page.url}\r\nreferer: ${REFERER}\r\n>\r\n`)
+        })))
+        fs.writeFile('./download.ef2', data.join(','), (err) => {
             if (err) throw err
-            console.log(err)
+            console.log('完成')
         })
     }
     static Aria2(infos) {
         let data = []
-        infos.map(info => {
-            info.map(pages => {
-                pages.map(page => {
-                    data.push(`${page.url}\r\n referer=${REFERER}\r\n dir=${BASEDIR}${page.dir}\r\n out=${page.part}.FLV\r\n`)
-                })
-            })
-        })
-        fs.appendFile('./download.txt', data.join(','), () => {
+        infos.map(info => info.map(pages => pages.map(page => {
+            data.push(`${page.url}\r\n referer=${REFERER}\r\n dir=${BASEDIR}${page.dir}\r\n out=${page.part}.FLV\r\n`)
+        })))
+        fs.writeFile('./download.txt', data.join(','), (err) => {
             if (err) throw err
-            console.log(err)
+            console.log('完成')
         })
     }
     static Aria2RPC(infos) {
-        infos.map(info => {
-            info.map(pages => {
-                pages.map(page => {
-                    rp({
-                        uri: ARIA2RPC,
-                        method: 'POST',
-                        body: {
-                            id: '',
-                            jsonrpc: 2,
-                            method: "aria2.addUri",
-                            params: [
-                                [page.url],
-                                { 'referer': `${REFERER}`, 'out': `${page.part}.FLV`, 'dir': `${BASEDIR}${page.dir}` }
-                            ]
-                        },
-                        json: true
-                    })
-                })
+        infos.map(info => info.map(pages => pages.map(page => {
+            rp({
+                uri: ARIA2RPC,
+                method: 'POST',
+                body: {
+                    id: '',
+                    jsonrpc: 2,
+                    method: "aria2.addUri",
+                    params: [
+                        [page.url],
+                        { 'referer': `${REFERER}`, 'out': `${page.out}.FLV`, 'dir': `${BASEDIR}${page.dir}` }
+                    ]
+                },
+                json: true
             })
-        })
+        })))
     }
-}
-
-class Type {
-    static async Vedio() {
-        let avs = readlineSync.question('请输入视频的 av 号（多个则以空格分隔）:').split(' ')
-        return Promise.all(avs.map(async av => {
-            let info = await Info.Vedio(av)
-            return info
-        }))
-    }
-    static async Bangumi() {
-        const bangumi = readlineSync.question('请输入番剧的名称和 season_id（以空格分隔）:').split(' ')
-        const name = bangumi[0], ss = bangumi[1]
-        let res = await Info.Bangumi(name, ss)
-        return [res]
-    }
-}
-
-function showList(infos) {
-    console.log(`\n[1] ALL`)
-    let index = 2
-    infos.map(info=> {
-        info.map(pages => {
-            pages.map(page => {
-                if (index > 9) { index = String.fromCharCode(97 - 10 + index) }
-                let title = page.part || page.title
-                console.log(`[${index}] ${title}`)
-                index++
-            })
-        })
-    })
-    console.log(`[0] CANCEL\n`)
 }
 
 (async () => {
-    // let wayNum = readlineSync.keyInSelect(['IDM', 'Aria2', 'Aria2RPC'], 'Please choose one way to exporter:')
-    let vedioType = readlineSync.keyInSelect(['单视频 部分/全 Part', '多视频 全 Part', '番剧 部分/全集'], '请选择一个视频类型:')
-    let infos;
-    if (parseInt(vedioType) == 0) {
-        // 单视频 部分 || 全 Part 
-        infos = await Type.Vedio()
-        console.log(infos[0][0][0].title)
-        showList(infos)
-        let avs = readlineSync.question('请选择需要下载的视频 Part 序号（多个则以空格分隔）:').split(' ')
-        if(avs.includes(0)) {
-            infos =  []
-        } else if(avs.includes(1)) {}
-        else {
+    const vedioType = readlineSync.keyInSelect(['视频', '番剧'], '请选择视频类型:')
+    if (vedioType == 0) return
+
+    let infos = await Info.get(vedioType)
+    Info.show(infos)
+
+    if (infos.length < 2) {
+        console.log('只有一个视频，可选择具体 Part 或集数。')
+        let avs = readlineSync.question('请选择需要下载的序号，1 为全部内容:').split(' ')
+        if (avs.includes('0')) return
+        if (!avs.includes('1')) {
             let res = [[]]
-            for(let i of avs) {
-                res[0].push(infos[0][i-2])
+            for (let i of avs) {
+                if (i < infos[0].length) {
+                    res[0].push(infos[0][i])
+                }
             }
             infos = res
-            console.log(res[0][0][0].part)
         }
-    } else if (parseInt(vedioType) == 1) {
-        // 多视频 全 Part
-        infos = await Type.Vedio()
-        console.log('所有视频名称：')
-        showList(infos)
-        let avs = readlineSync.question('请选择需要下载的视频序号（多个则以空格分隔）:').split(' ')
-        if(avs.includes(0)) {
-            infos =  []
-        } else if(avs.includes(1)) {}
-        else {
-            let res = []
-            for(let i of avs) {
-                res.push(infos[i-2])
-            }
-            infos = res
-            console.log(res[0][0][0].title)
-        }
-    } else if (parseInt(vedioType) == 2) {
-        // 单番剧 部分 || 全集
-        infos = await Type.Bangumi()
-        console.log(infos)
-        console.log('所有集名称：')
-        showList(infos)
     }
 
-    // switch (wayNum) {
-    //     case 0: this.IDM(info); break;
-    //     case 1: this.Aria2(info); break;
-    //     case 2: this.Aria2RPC(info); break;
-    //     default: break;
-    // }
+    const exporterType = readlineSync.keyInSelect(['IDM', 'Aria2', 'Aria2RPC'], '请选择导出方式:')
+    switch (parseInt(exporterType)) {
+        case 0: Exporter.IDM(infos); break;
+        case 1: Exporter.Aria2(infos); break;
+        case 2: Exporter.Aria2RPC(infos); break;
+        default: break;
+    }
 })()
